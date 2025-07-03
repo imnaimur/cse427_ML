a1f1401ff5924a28926ea9c2b5ffd372618268a3
Naimur Rahman <87676737+imnaimur@users.noreply.github.com>
2025-07-03 20:25:54 +0600

    done

--- labtask.py
+++ labtask.py
@@ -34,3 +34,43 @@ print(df.isnull().sum())
 print(df.value_counts("Math"))
 df.fillna( df['Physics'].mean(), inplace=True)
 print(df)
+
+# matplotlib
+import matplotlib.pyplot as plt
+
+temperature_dhaka = np.array([25,34,21,45,28,6,43,18,7,2])
+humidity_dhaka = np.array([28, 25,29,20, 26, 50, 19, 29, 52, 55])
+
+plt.scatter(temperature_dhaka,humidity_dhaka,marker = "*")
+plt.title("temperature_dhaka vs humidity_dhaka")
+plt.show()
+
+study_hours = np.array([2,3,4,4, 5, 6, 7, 7, 8, 9, 9, 10, 11, 11, 12])
+marks1 = np.array([6, 10, 15, 20, 34, 44, 55, 60, 55, 67, 70, 80, 90, 99, 100])
+plt.figure(figsize=(12,8))
+plt.title("study_hours vs marks")
+plt.plot(study_hours,marks1,linestyle = "solid")
+
+plt.show()
+
+subjects = np.array(['Maths', 'English', 'Science', 'Physics', 'Computer'])
+marks2 = np.array([89, 90, 45, 78, 99])
+plt.title("Subjects vs marks")
+plt.barh(subjects,marks2)
+plt.show()
+plt.bar(subjects,marks2)
+plt.show()
+
+plt.subplot(4,1,1)
+plt.scatter(temperature_dhaka,humidity_dhaka,marker = "*")
+
+plt.subplot(4,1,2)
+plt.plot(study_hours,marks1)
+
+plt.subplot(4,1,3)
+plt.barh(subjects,marks2)
+
+plt.subplot(4,1,4)
+plt.bar(subjects,marks2)
+
+plt.show()
